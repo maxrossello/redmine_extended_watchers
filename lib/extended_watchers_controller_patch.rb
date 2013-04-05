f632460b816ef33be9fc9d550985dc95ae54a591
Massimo Rossello <massimo.rossello@azcom.it>
2013-04-05 09:58:19 +0200

    First commit

---
+++ lib/extended_watchers_controller_patch.rb
@@ -0,0 +1,28 @@
+require_dependency 'watchers_controller'
+
+module ExtendedWatchersControllerPatch
+    
+    def self.included(base)
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+            unloadable
+
+            alias_method_chain :autocomplete_for_user, :extwatch
+        end
+    end
+
+    module InstanceMethods
+
+        def autocomplete_for_user_with_extwatch
+          @users = @project.users.sort
+          @users.reject! {|user| !user.allowed_to?(:view_issues, @project)}
+          if @watched
+            @users -= @watched.watcher_users
+          end
+          render :layout => false
+        end
+
+    end
+
+end
+
