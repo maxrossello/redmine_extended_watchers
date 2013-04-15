b3ac2ea72c607fd6f18325bf7c9fcfef8b306f16
Massimo Rossello <massimo.rossello@azcom.it>
2013-04-15 11:54:05 +0200

    2.1 and following support

--- plugins/redmine_extended_watchers/init.rb
+++ plugins/redmine_extended_watchers/init.rb
@@ -3,16 +3,11 @@ require 'redmine'
 Rails.logger.info 'Starting Extended Watchers plugin for Redmine'
 
 require_dependency 'extended_watchers_issue_patch'
-require_dependency 'extended_watchers_journal_patch'
 require_dependency 'extended_watchers_controller_patch'
 
 Rails.configuration.to_prepare do
   unless Issue.included_modules.include?(ExtendedWatchersIssuePatch)
       Issue.send(:include, ExtendedWatchersIssuePatch)
-  end
-
-  unless Journal.included_modules.include?(ExtendedWatchersJournalPatch)
-      Journal.send(:include, ExtendedWatchersJournalPatch)
   end
 
   unless WatchersController.included_modules.include?(ExtendedWatchersControllerPatch)
@@ -25,8 +20,8 @@ Redmine::Plugin.register :redmine_extended_watchers do
   name 'Redmine Extended Watchers plugin'
   author 'Massimo Rossello'
   description 'Allows users that can view at least own and assigned issues in a project to be notified of some issue if set as watchers (making patch #8488 a plugin)'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/maxrossello/redmine_extended_watchers.git'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version_or_higher => '1.4.0'
+  requires_redmine :version_or_higher => '2.1.0'
 end
--- plugins/redmine_extended_watchers/lib/extended_watchers_issue_patch.rb
+++ plugins/redmine_extended_watchers/lib/extended_watchers_issue_patch.rb
@@ -7,43 +7,55 @@ module ExtendedWatchersIssuePatch
         base.class_eval do
             unloadable
 
-            named_scope :visible, lambda {|*args| { :include => [:project, :watchers],
-                                                    :conditions => Issue.visible_condition(args.shift || User.current, *args) } }
-
             alias_method_chain :visible?, :extwatch
         end
 
         base.instance_eval do
           def visible_condition(user, options={})
             Project.allowed_to_condition(user, :view_issues, options) do |role, user|
-              case role.issues_visibility
+              # Keep the code DRY
+              if [ 'default', 'own' ].include?(role.issues_visibility)
+                user_ids = [user.id] + user.groups.map(&:id)
+                watched_issues = Issue.watched_by(user).map(&:id)
+                watched_issues_clause = watched_issues.empty? ? "" : " OR #{table_name}.id IN (#{watched_issues.join(',')})"
+              end
+
+              if user.logged?
+                case role.issues_visibility
                 when 'all'
                   nil
                 when 'default'
-                  user_ids = [user.id] + user.groups.map(&:id)
-                  "(#{table_name}.is_private = #{connection.quoted_false} OR #{table_name}.author_id = #{user.id} OR #{Watcher.table_name}.user_id = #{user.id} OR #{table_name}.assigned_to_id IN (#{user_ids.join(',')}))"
+                  "(#{table_name}.is_private = #{connection.quoted_false} OR #{table_name}.author_id = #{user.id} OR #{table_name}.assigned_to_id IN (#{user_ids.join(',')}) #{watched_issues_clause})"
                 when 'own'
-                  user_ids = [user.id] + user.groups.map(&:id)
-                  "(#{table_name}.author_id = #{user.id} OR #{Watcher.table_name}.user_id = #{user.id} OR #{table_name}.assigned_to_id IN (#{user_ids.join(',')}))"
+                  "(#{table_name}.author_id = #{user.id} OR #{table_name}.assigned_to_id IN (#{user_ids.join(',')}) #{watched_issues_clause})"
                 else
                   '1=0'
                 end
+              else
+                "(#{table_name}.is_private = #{connection.quoted_false})"
+              end
             end
+            
           end
-
         end
     end
 
     module InstanceMethods
         def visible_with_extwatch?(usr=nil)
           (usr || User.current).allowed_to?(:view_issues, self.project) do |role, user|
-            case role.issues_visibility
+            if user.logged?
+              case role.issues_visibility
+              when 'all'
+                true
               when 'default'
-                !self.is_private? || self.author == user || self.watched_by?(user) || user.is_or_belongs_to?(assigned_to)
+                !self.is_private? || (self.author == user || self.watched_by?(user) || user.is_or_belongs_to?(assigned_to))
               when 'own'
                 self.author == user || self.watched_by?(user) || user.is_or_belongs_to?(assigned_to)
               else
                 visible_without_extwatch?(usr)
+              end
+            else
+              visible_without_extwatch?(usr)
             end
           end
         end
