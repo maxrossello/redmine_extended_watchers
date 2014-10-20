48bfcc7a7df559fd8c7687e7688c1126324b5723
Massimo Rossello <massimo.rossello@azcom.it>
2014-10-20 14:01:44 +0200

    Performance improvement, #6

--- lib/extended_watchers_application_controller_patch.rb
+++ lib/extended_watchers_application_controller_patch.rb
@@ -14,13 +14,11 @@ module ExtendedWatchersApplicationControllerPatch
 
     def authorize_with_extwatch(ctrl = params[:controller], action = params[:action], global = false)
       if (ctrl == "projects" && action == "show")
-        Issue.watched_by(User.current).all.each do |issue|
-          if issue.project == @project
-            unless User.current.allowed_to?({:controller => ctrl, :action => action}, @project || @projects, :global => global)
-              redirect_to _project_issues_path(@project)
-            end
-            return true
+        Issue.where(:project_id => @project).watched_by(User.current).all.each do |issue|
+          unless User.current.allowed_to?({:controller => ctrl, :action => action}, @project || @projects, :global => global)
+            redirect_to _project_issues_path(@project)
           end
+          return true
         end
       elsif (ctrl == "issues" && action == "show")
         return true if Issue.joins(:project => :enabled_modules).where("#{EnabledModule.table_name}.name = 'issue_tracking'").find(params[:id]).watched_by?(User.current)
